380184e139ce21b239682e80ef1a366604d38131
pavelbeard <weewlol1337@gmail.com>
2022-08-18 13:47:54 +0300

    v1.0.4 - скрипт установки доработан

--- main_vlan.py
+++ main_vlan.py
@@ -1,7 +1,12 @@
-import sys
-import preproc
 import os
+
 from cisco_functions import CiscoDataHandler
+
+
+def execute():
+    c = CiscoDataHandler()
+    c.get_info(dev_info)
+
 
 os.environ["NTC_TEMPLATES_DIR"] = os.getcwd() + '\\NTC_TEMPLATES'
 
@@ -20,7 +25,12 @@ if (
         dev_info['password'] == '********' or
         dev_info['secret'] == '********'
 ):
-    print("Введи логин и пароль в конфиг устройства")
+    print("Введи логин")
+    dev_info['username'] = input()
+    print("...и пароль:")
+    password = input()
+    dev_info['password'] = password
+    dev_info['secret'] = password
+    execute()
 else:
-    c = CiscoDataHandler()
-    c.get_info(dev_info)
+    execute()
