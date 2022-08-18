43028d8982f237cadbb9c3fed2dea852716483cf
pavelbeard <weewlol1337@gmail.com>
2022-08-18 10:06:50 +0300

    v1.0.2 - install script is done

--- install.ps1
+++ install.ps1
@@ -1,15 +1,44 @@
+﻿Add-Type -AssemblyName PresentationFramework
+
 $python_path = $env:path
+$run = $PSScriptRoot + "\run.ps1"
+
 if ($python_path -like "*Python*")
 {
-    $run = $PSScriptRoot + "\run.ps1"
+    CreateLink
+}
+else
+{
+    # установка python, если нет его на машине
+    $python_exe = "$PSScriptRoot\python_installer"
+    Set-Location -Path $python_exe
+
+    try
+    {
+        Start-Process -FilePath "$python_exe\python-3.10.5-amd64.exe" -Verb RunAs -Wait
+
+        CreateLink
+
+        [System.Windows.MessageBox]::Show(
+            "Установка завершена.
+            Ярлык скрипта vlan_mac_compiler находится на рабочем столе.")
+    }
+    catch [InvalidOperationException]
+    {
+        [System.Windows.MessageBox]::Show(
+            "Установка прервана пользователем")
+    }
+#    catch [AuthenticationException]
+#    {
+#        [System.Windows.MessageBox]::Show(
+#            "Неверные данные пользователя")
+#    }
+}
+
+function CreateLink {
     $ShortcutPath = "C:\Users\$env:UserName\Desktop\vlan_mac_compiller.lnk"
     $WScriptObj = New-Object -ComObject ("WScript.Shell")
     $shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
     $shortcut.TargetPath = $run
     $shortcut.Save()
-
-}
-else
-{
-
 }
